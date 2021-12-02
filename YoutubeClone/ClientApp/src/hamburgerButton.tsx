a0690770fe57ac76c36c66890f30a3a84ddb184e
Thomas Mathers <thomas.mathers@ideanotion.net>
2021-12-02 17:16:05 -0500

    changes

--- YoutubeClone/ClientApp/src/hamburgerButton.tsx
+++ YoutubeClone/ClientApp/src/hamburgerButton.tsx
@@ -1,4 +1,4 @@
-import { Button, IconButton } from "@mui/material";
+import { IconButton } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 
 function HamburgerButton() {
--- YoutubeClone/ClientApp/src/logo.tsx
+++ YoutubeClone/ClientApp/src/logo.tsx
@@ -2,7 +2,7 @@ import { Stack, Typography } from "@mui/material";
 
 const Logo = () => (
     <Stack direction="row" spacing={1} alignItems="center" paddingLeft={1} paddingRight={1}>
-        <img src="logo512.png" height={40} />
+        <img src="logo512.png" alt="logo" height={40} />
         <Typography>YoutubeClone</Typography>
     </Stack>
 );
